9fab85411f98e6bbd8104b0cac9d0b1c98943a0f
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-06-21 23:33:24 +0530

    string_methods

---
+++ Javascript/string.js
@@ -0,0 +1,10 @@
+
+
+
+console.log("I am javascript")
+let firststring = "html css javascript in web web web  development"
+console.log(firststring.length)
+console.log(firststring.indexOf("css"))
+console.log(firststring.lastIndexOf("web"))
+console.log(firststring.substring(5,10))
+console.log(firststring.slice(5,10))  //slice() extracts a part of a string and returns the extracted part in a new string.
