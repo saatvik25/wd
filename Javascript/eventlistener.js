1f94361d4de80e37fe22cdc62e24944afbea5852
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-06-22 01:32:23 +0530

    Event Listener

---
+++ Javascript/eventlistener.js
@@ -0,0 +1,19 @@
+
+
+
+function clicked(){
+    console.log("burron is clicked")
+
+}
+window.onload = function(){
+    console.log("page is loaded")
+}
+     //event listner -> "click", mousover
+firstContainer.addEventListener('click' , function(){
+    document.querySelectorAll()
+    console.log("click is done");
+})
+
+firstContainer.addEventListener('mouseover' , function(){   //firstContainer IS ID WE ADD  ID RO EVENT LISTNer 
+    console.log("click is done");
+}) 
