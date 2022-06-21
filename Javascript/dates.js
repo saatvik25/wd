78cb40e3e07defd44d839827b7a7ed2aad31ca6e
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-06-22 01:28:16 +0530

    javascript dates

---
+++ Javascript/dates.js
@@ -0,0 +1,12 @@
+
+
+
+let d = new Date();
+// console.log("today date is ",d);
+
+console.log(d.getDay());  //count start from 0 (sunday).......etc
+console.log(d.getFullYear())
+console.log(d.getTime())
+console.log(d.getHours())
+const date = d;
+document.getElementById("demo").innerHTML = d;
