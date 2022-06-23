145cef589c7e2dca828d79dfa10dbd4ec80ef438
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-06-23 14:56:12 +0530

    DOM MANIPULATION

---
+++ Javascript/DOM MANIPULATION.js
@@ -0,0 +1,24 @@
+
+
+
+let elem  = document.getElementById("btn");
+console.log(elem)
+
+let c = document.getElementsByClassName("container");
+console.log(c)
+
+// c[0].style.background = "black" 
+c[0].classList.add("container2")
+
+console.log(elem.innerHTML);
+console.log(c[0 ].innerText)
+
+d =document.getElementsByTagName("div");
+console.log(d);
+
+
+//selecting using query selector
+
+sel = document.querySelector(".container");
+console.log(sel)
+
