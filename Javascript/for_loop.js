04a3810a6a4128faf94f25c4fbba9a5c95238636
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-06-24 09:19:53 +0530

    loop.js

---
+++ Javascript/for_loop.js
@@ -0,0 +1,15 @@
+
+// foor loops
+
+let text="";
+const alpha = ["a","b","c","d","e","f"];
+for(let i=0;i<alphamlength;i++){
+    text += cars[i]+"<br>";
+}
+
+
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+let i, len, textt;
+for (i = 0, len = cars.length, textt = ""; i < len; i++) {
+  text += cars[i] + "<br>";
+}
