84dcb9e3092853c0685d4faccce9fef44eefc9fa
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-02 00:12:40 +0530

    Create function_1.js

---
+++ Javascript/function_1.js
@@ -0,0 +1,21 @@
+function main(){
+   mov();
+   mov();
+   mov();
+   mov();
+}
+
+function mov(){
+   turnLeft();
+   move();
+   turnRight();
+   move();
+   putBeeper();
+}
+
+function mov4(){
+   move();
+   move();
+   move();
+   move();
+}
