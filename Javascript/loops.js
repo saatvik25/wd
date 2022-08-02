c59259392ffb293fb345f5f93e2706b3851ae3ce
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-02 23:25:54 +0530

    loops

---
+++ Javascript/loops.js
@@ -0,0 +1,15 @@
+const arr = ['11','33','34','88'];
+
+//for loop
+var i;
+for(i =0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+var j= arr.length;
+
+
+//while loop
+while(j>=0){
+    console.log(arr[j]);
+    j =j-1;
+}
