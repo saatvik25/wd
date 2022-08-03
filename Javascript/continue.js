607f08cc6a7d5b3b72d62dae30f500562bd15f3d
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-03 23:36:37 +0530

    Create continue.js

---
+++ Javascript/continue.js
@@ -0,0 +1,7 @@
+//The continue statement breaks one iteration (in the loop), if a specified condition occurs, and continues with the next iteration in the loop.
+
+
+for (let i = 0; i < 10; i++) {
+  if (i === 3) { continue; }
+  console.log(i) ;
+}
