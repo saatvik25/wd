60fc2d43a077ea07ec7bda06df17cba67d00fc65
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-03 13:06:19 +0530

    JavaScript Operators

---
+++ Javascript/operators.js
@@ -0,0 +1,12 @@
+// JavaScript Operators
+let x = 5;         // assign the value 5 to x
+let y = 2;         // assign the value 2 to y
+let z = x + y; 
+console.log("x = "+x); // assign the value 7 to z (5 + 2)
+console.log("y = "+y);
+console.log("x+y = "+z);
+
+let a = 5;
+let b = 2;
+let q = a*b;
+console.log(z)
