d615516315d2ce724087c013d30a715b14be5c3e
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-03 13:20:29 +0530

    sort

---
+++ Javascript/sort.js
@@ -0,0 +1,12 @@
+
+// Sorting an Array
+// The sort() method sorts an array alphabetically
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const final_list = fruits.sort();
+console.log(final_list);
+
+// Numeric Sort
+
+const points = [40, 100, 1, 5, 25, 10];
+console.log(points.sort());
