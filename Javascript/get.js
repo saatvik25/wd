0fcd2ee6c93a9e07acb05786964f5408d3a6e860
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-03 23:40:42 +0530

    Create get.js

---
+++ Javascript/get.js
@@ -0,0 +1,9 @@
+// The get() method gets the value of a key in a Map:
+
+
+const fruits = new Map([
+  ["apples", 500],
+  ["bananas", 300],
+  ["oranges", 200]]);
+  var a = fruits.get("bananas");
+  console.log(a);
