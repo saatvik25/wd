8bff1d6c485f14b386e4d54550ab5397f2f47902
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-03 12:50:56 +0530

    Random

---
+++ Javascript/Random.js
@@ -0,0 +1,11 @@
+// javaScript Random
+// Math.random() returns a random number between 0 (inclusive),  and 1 (exclusive):
+console.log(Math.random());
+
+
+// Returns a random integer from 0 to 9:
+console.log(Math.floor(Math.random() * 10));
+
+
+// Returns a random integer from 0 to 99:
+console.log(Math.floor(Math.random() * 100));
