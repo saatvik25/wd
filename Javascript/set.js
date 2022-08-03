d89ddd0fd6b0e06837076c54e10d093ea9e6b0ea
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-03 12:54:56 +0530

    set

---
+++ Javascript/set.js
@@ -0,0 +1,8 @@
+// The new Set() Method
+// Create a Set
+const letters = new Set();
+
+// Add Values to the Set
+console.log(letters.add("a"));
+console.log(letters.add("b"));
+console.log(letters.add("c"));
