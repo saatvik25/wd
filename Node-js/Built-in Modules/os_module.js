0a1a6ee8e9d0544a89a8c34483500a7b5a5723c2
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-04 20:37:52 +0530

    os_module

---
+++ Node-js/Built-in Modules/os_module.js
@@ -0,0 +1,13 @@
+const os = require('os');
+
+console.log(os.freemem());
+
+console.log(os.userInfo);
+
+console.log("system uptime is " ,os.uptime()," second");
+
+console.log(os.version());
+
+console.log(os.homedir());
+
+console.log(os.hostname());
