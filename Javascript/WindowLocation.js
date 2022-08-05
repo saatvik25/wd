39917e0ac4c28f54f45477d799a911cabe60c236
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-05 19:46:29 +0530

    Window Location

---
+++ Javascript/WindowLocation.js
@@ -0,0 +1,22 @@
+
+
+
+//The location object contains information about the 
+//current URL.
+
+//The location object is accessed with:
+//window.location or just location
+
+
+console.log(window.location.origin);
+
+
+//Location Object Properties
+
+//The location.hash property sets or returns the anchor //part of a URL, including the hash sign (#).
+
+location.hash
+
+//The location.hostname property returns the host (IP //adress or domain) of a URL.
+
+console.log(location.hostname);
