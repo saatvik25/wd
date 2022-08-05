801354c3a03b5664d0cc0bd48408092b2e5bb5bb
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-05 19:44:58 +0530

    Navigator Object Methods

---
+++ Javascript/Navigator.js
@@ -0,0 +1,8 @@
+//The javaEnabled() method returns a Boolean value 
+//that specifies whether the browser has Java enabled.
+
+let java = navigator.javaEnabled();
+console.log(java)
+
+true if the browser has Java enabled, otherwise false.
+
