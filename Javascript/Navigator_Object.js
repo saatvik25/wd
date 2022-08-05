5e2d36a31253f3dc70ec5b4d74331513610a9d42
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-05 19:44:05 +0530

    Window Navigator Object

---
+++ Javascript/Navigator_Object.js
@@ -0,0 +1,20 @@
+
+
+
+//The navigator object contains information about the browser
+
+//give browser app name
+
+console.log(navigator.appName);
+
+
+// give app version
+console.log(navigator.appVersion);
+
+//give code name of browser
+
+console.log(navigator.appCodeName);
+
+
+//Cookies enabled in your browser?
+navigator.cookieEnabled;
