7e8b3597d49e57fac536bdf0e7aa582330e3432e
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-05 19:42:29 +0530

    Javascript window

---
+++ Javascript/window.js
@@ -0,0 +1,18 @@
+//Javascript window
+
+
+
+//Window alert()
+confirm("Press a button!");
+
+//confirm("Press a button!");
+// gives two options ok and cancel
+confirm("Press a button!");
+
+//prompt
+//take value from user
+prompt('enter name')
+
+//open
+//open url
+window.open('any url')
