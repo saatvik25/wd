697a4adfa2e802a75a3a58658fb4b13d12ce3a18
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-05 19:43:18 +0530

    The Window Screen Object

---
+++ Javascript/Screen_Object.js
@@ -0,0 +1,22 @@
+
+
+
+//The Window Screen Object
+//The screen object contains information about the 
+//visitor's screen.
+
+//height of screen
+//returns the height in pixels.
+
+console.log(screen.availHeight);
+
+//returns the width of the user's screen.
+console.log(screen.availWidth);
+
+
+//console.log(screen.colorDepth);
+
+console.log(screen.colorDepth);
+
+//Get the total height of your screen:
+console.log(screen.height);
