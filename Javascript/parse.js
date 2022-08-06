77cba4ba60337014cf10654560e9f360b96fa1ae
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-06 20:25:09 +0530

    Convert data from json to javascript

---
+++ Javascript/parse.js
@@ -0,0 +1,16 @@
+
+
+
+//Convert data from json to javascript
+
+JSON.parse()
+
+const  myjson=  {“name":"John", "age":30, "city":"New York"}'
+
+const jsobj = JSON.parse('{"name":"John", "age":30, "city":"New York"}');
+
+
+Var obj  = '{"name":"John", "age":30, "city":"New York"}'
+
+
+Console.log(JSON.parse(obj));
