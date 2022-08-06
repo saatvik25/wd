3ce51ba315c5eb5a4a7657401452f85a7b890724
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-06 20:21:41 +0530

    Create j.js

---
+++ Javascript/j.js
@@ -0,0 +1,23 @@
+
+
+
+//JSON data is written as name/value pairs (aka key/value pairs).
+
+"name":"John"
+
+
+//In JSON, keys must be strings, written with double quotes:
+
+{"name":"John"}
+
+//In JavaScript, keys can be strings, numbers, or identifier names:
+
+{name:"John"} 
+
+// JSON Example
+
+{"employees":[
+  { "firstName":"John", "lastName":"Doe" },
+  { "firstName":"Anna", "lastName":"Smith" },
+  { "firstName":"Peter", "lastName":"Jones" }
+]}
