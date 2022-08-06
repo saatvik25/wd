b3d3cddaf0212599462b3e0a1c1f561229090b6f
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-06 20:22:22 +0530

    JavaScript Objects

---
+++ Javascript/Objects.js
@@ -0,0 +1,16 @@
+//JavaScript Objects
+
+//with JavaScript you can create an object and assign data to it, like this:
+
+
+Var person = {name:"John", age:31, city:"New York"};
+
+
+// You can access a JavaScript object like this
+
+person.name
+
+
+// Data can be modified like this:
+
+person.name = "Gilbert";
