21a66ef415a0526438235a596603e62b9dcedb54
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-06 20:16:39 +0530

    Window History Object

---
+++ Javascrip/Window History Object.js
@@ -0,0 +1,18 @@
+
+
+
+//The Window History Object
+
+//The history object contains the URLs visited by the //user
+
+//The history object is accessed with
+
+window.history or just history
+
+
+//Window history.back()
+
+history.back()
+
+
+history.forward()
