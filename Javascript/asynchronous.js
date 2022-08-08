ddee6333bedab3b46dbf013074256f586be3c20a
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-08-09 01:43:53 +0530

    Create asynchronous.js

---
+++ Javascript/asynchronous.js
@@ -0,0 +1,11 @@
+setTimeout(function(){
+    for(var j = 1; j<10;j++){
+        console.log("printing "+i);
+        
+       }
+       
+       
+       
+},1000);
+
+console.log("printing is done");
